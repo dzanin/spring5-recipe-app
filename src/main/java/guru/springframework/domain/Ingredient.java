57eb1619699ca212e89a4dcf42d8615d3665a084
dzanin <davide.zanin@supsi.ch>
2021-03-14 23:49:05 +0100

    one-to-one-jpa-assign-review

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -14,7 +14,9 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    //private UnitOfMeasure uom;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
@@ -48,5 +50,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
