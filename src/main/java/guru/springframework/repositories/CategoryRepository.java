1729e4bce476a76dfcf08df2fa19e443f469516a
dzanin <davide.zanin@supsi.ch>
2021-03-15 09:52:40 +0100

    jpa-query-method

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -4,8 +4,12 @@ import guru.springframework.domain.Category;
 import guru.springframework.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by dz on 15/03/21.
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
+
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -2,10 +2,13 @@ package guru.springframework.repositories;
 
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
-import sun.tools.jconsole.Plotter;
+
+import java.util.Optional;
 
 /**
  * Created by dz on 15/03/21.
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
